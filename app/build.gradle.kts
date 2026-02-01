4637fe2cbb733e908e9fa77f9a47d5b43b6cb496
suyunkai <cn.syk@foxmail.com>
2026-02-01 23:31:22 +0800

    小bug修复

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 12
-        versionName = "0.9.9-test-02012157"
+        versionName = "0.9.9-test-02012329"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
